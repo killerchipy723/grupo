b289f7f7ae2e666ab32054cf9f5c2f907c0da955
Diego Alderete <141648675+killerchipy723@users.noreply.github.com>
2025-04-11 11:46:22 -0300

    genial

--- app.py
+++ app.py
@@ -6,7 +6,47 @@ app = Flask(__name__)
 #Ruta Principa 
 @app.route("/",methods=['GET','POST'])
 def principal():
-    return render_template("index.html")
+    if request.method=='GET':
+        conexion = db_getConnection()
+        query = "select * from alumno"
+        cursor = conexion.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+    return render_template("index.html",alumno=data)
+
+@app.route("/Evento",methods=['POST'])
+def reg_Evento():
+    conexion = db_getConnection()
+    if request.method=='POST':
+        nombre = request.form['nombre']
+        fecha = request.form['fecha']        
+        query = "INSERT INTO evento(nombre,fecha)VALUES(%s,%s)"
+        cursor = conexion.cursor()
+        cursor.execute(query,(nombre,fecha))
+        conexion.commit()
+        print("Registro Exitoso")
+        return redirect(url_for('principal'))
+    else:
+        print("Error al insertar el registro")
+
+
+@app.route("/RegGrupo",methods=['POST'])
+def reg_Grupo():
+        conexion = db_getConnection()
+        if request.method=="POST":
+            idalumno = request.form['alumno']
+            nombre = request.form['nombre']
+            edad = request.form['edad']
+            es = request.form['tipo']
+            query = "INSERT INTO familia(idalumno,nombre,edad,es)VALUES(%s,%s,%s,%s)"
+            cursor = conexion.cursor()
+            cursor.execute(query,(idalumno,nombre,edad,es))
+            conexion.commit()
+            print("Registro Insertado")
+            return redirect(url_for('principal'))
+        else:
+            print("Error al Guardar el Registro")
+       
 
 #Ruta para insertar alumnos
 @app.route("/Registro",methods=['POST'])
@@ -27,6 +67,11 @@ def reg_Alumno():
         return redirect(url_for('principal'))
     else:
         print("Error al Guardar el Registro")
+    
+    # Agregar Personas
+    
+    
+
 
 
 
